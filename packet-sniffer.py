7f4767df6ed796e22712254c74ecc672f7c3ab1d
Eiyarrr <eiyar@eiyar.com>
2026-04-08 19:32:38 +0000

    Changed name, 'range' is already a keyword

--- packet-sniffer.py
+++ packet-sniffer.py
@@ -5,10 +5,10 @@ import sys
 def print_packet(packet):
     return packet.summary()
 
-def packet_loop(range):
+def packet_loop(user_range):
     print("start")
     prev = 0
-    for _ in range(1, range):
+    for _ in range(1, user_range):
         print("loop")
         capture = sniff(prn=print_packet, count = 1)
         if capture == prev: # -> "[x#]" at the end of packet summaries
@@ -21,21 +21,21 @@ def parse_argv():
         print("Invalid number of arguments")
         sys.exit(-1)
 
-    range = sys.argv[1]
+    user_range = sys.argv[1]
     try:
-        range = int(range)
+        user_range = int(user_range)
     except ValueError:
         print("Range must be an integer")
         sys.exit(-1)
 
-    if range <= 0:
+    if user_range <= 0:
         print("Range must be > 0")
         sys.exit(-1)
-    return range
+    return user_range
 
 def main():
-    range = parse_argv()
-    packet_loop(range)
+    user_range = parse_argv()
+    packet_loop(user_range)
 
 if __name__ == "__main__":
     main()
