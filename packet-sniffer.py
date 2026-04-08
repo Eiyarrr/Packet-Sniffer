b254cb80c84e3113fcf95a66562ab96ae66e67d7
Eiyarrr <eiyar@eiyar.com>
2026-04-08 18:19:17 +0000

    Added basic user arg (range)

--- packet-sniffer.py
+++ packet-sniffer.py
@@ -1,13 +1,14 @@
 from scapy.all import sniff
 import time
+import sys
 
 def print_packet(packet):
     return packet.summary()
 
-def packet_loop():
+def packet_loop(range):
     print("start")
     prev = 0
-    for _ in range(0, 10):
+    for _ in range(1, range):
         print("loop")
         capture = sniff(prn=print_packet, count = 1)
         if capture == prev: # -> "[x#]" at the end of packet summaries
@@ -15,8 +16,25 @@ def packet_loop():
         prev = capture
         time.sleep(0.1)
 
+def parse_argv():
+    if len(sys.argv) != 2:
+        print("Invalid number of arguments")
+        sys.exit(-1)
+
+    range = sys.argv[1]
+    try:
+        range = int(range)
+    except ValueError:
+        print("Range must be an integer")
+
+    if int(range) <= 0:
+        print("Range must be > 0")
+        sys.exit(-1)
+    return range
+
 def main():
-    packet_loop()
+    range = parse_argv()
+    packet_loop(range)
 
 if __name__ == "__main__":
     main()
