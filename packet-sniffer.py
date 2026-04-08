55eeae7ea82ed080888f4a842219ff53e6b46d30
Eiyarrr <eiyar@eiyar.com>
2026-04-08 18:12:02 +0000

    set everything into functions

--- packet-sniffer.py
+++ packet-sniffer.py
@@ -4,12 +4,19 @@ import time
 def print_packet(packet):
     return packet.summary()
 
-print("start")
-prev = 0
-for _ in range(0, 10):
-    print("loop")
-    capture = sniff(prn=print_packet, count = 1)
-    if capture == prev: # -> "[x#]" at the end of packet summaries
-        print(1)
-    prev = capture
-    time.sleep(0.1)
+def packet_loop():
+    print("start")
+    prev = 0
+    for _ in range(0, 10):
+        print("loop")
+        capture = sniff(prn=print_packet, count = 1)
+        if capture == prev: # -> "[x#]" at the end of packet summaries
+            print(1)
+        prev = capture
+        time.sleep(0.1)
+
+def main():
+    packet_loop()
+
+if __name__ == "__main__":
+    main()
