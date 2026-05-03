a1fe352b4fea0ba1d0daf9e355c40b54e5f6b58c
Eiyarrr <eiyar@eiyar.com>
2026-05-03 02:36:27 +0000

    Added better parsing of arguments into the fields list

--- user_input.py
+++ user_input.py
@@ -5,6 +5,13 @@ import argparse
 def parse_user_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
+        "quantity",
+        nargs="?",
+        help="The quantity of packets to give (default = 50)",
+        type=int,
+        default=50,
+    )
+    parser.add_argument(
         "-src",
         help="Include the source IP of the packet when printing summaries",
         action="store_true",
@@ -19,26 +26,15 @@ def parse_user_args():
         help="Include the IP protocol of the packet when printing summaries",
         action="store_true",
     )
-    parser.add_argument(
-        "quantity",
-        nargs="?",
-        help="The quantity of packets to give (default = 50)",
-        type=int,
-        default=50,
-    )
 
     return parser.parse_args()
 
 
 def get_fields():
     args = parse_user_args()
-    fields = [args.quantity]
+    fields = []
 
-    if args.src:
-        fields.append("IP.src")
-    if args.dst:
-        fields.append("IP.src")
-    if args.proto:
-        fields.append("IP.src")
+    for a in vars(args).values():
+        fields.append(a)
 
     return fields
