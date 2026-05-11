7f3b4774a5cb42b223d5834859271925f315042e
Eiyarrr <eiyar@eiyar.com>
2026-05-11 06:04:55 +0000

    feat: added arg for dns resolution

--- user_input.py
+++ user_input.py
@@ -4,31 +4,36 @@ import argparse
 # Also must make more options (presets?)
 def parse_user_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
+    parser.add_argument( # 0
         "quantity",
         nargs="?",
         help="The quantity of packets to give (default = 50)",
         type=int,
         default=50,
     )
-    parser.add_argument(
+    parser.add_argument( # 1
         "-p",
         help="Print output of sniffer to terminal",
         action="store_true",
     )
-    parser.add_argument(
+    parser.add_argument( # 2
         "-src",
         help="Include the source IP of the packet when printing summaries",
         action="store_true",
     )
-    parser.add_argument(
+    parser.add_argument( # 3
         "-dst",
         help="Include the destination IP of the packet when printing summaries",
         action="store_true",
     )
-    parser.add_argument(
+    parser.add_argument( # 4
         "-proto",
         help="Include the IP protocol of the packet when printing summaries",
+        action="store_true",
+    )
+    parser.add_argument( # 5
+        "-dns",
+        help="parse IP addresses in printouts to domains",
         action="store_true",
     )
 
