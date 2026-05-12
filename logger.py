1789144149421ce53d7bb71fd3bf67504c922932
Eiyarrr <eiyar@eiyar.com>
2026-05-12 04:31:24 +0000

    feat: added scapy's wrpcap and changed logging to pcap

--- logger.py
+++ logger.py
@@ -1,10 +1,10 @@
 from sniffer import seen
-import json
+from scapy.all import wrpcap
+
 
 def write_data():
     data = []
     for s in seen:
         data.append((seen[s]["packet"], seen[s]["count"]))
-
-    file = open("packets.json", "w")
-    json.dump(data, file, indent=4)
+    
+    wrpcap("capture.pcap", data)
